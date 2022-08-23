c44006ecbb3cc5a61b8febf02364692d1db1b364
Olgrin <matrica_1997@mail.ru>
2022-08-23 22:38:09 +0300

    add Homework 3

---
+++ Homework3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Ведите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+
+Console.Write("Наибольшее число ");
+Console.Write(max);
