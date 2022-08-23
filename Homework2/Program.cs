48d190f7ef0e92c47f05e5ca452a0ae8f7ef3160
Olgrin <matrica_1997@mail.ru>
2022-08-23 22:31:27 +0300

    add Homework 2

---
+++ Homework2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ведите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a>b)
+{
+    Console.Write("Больше число ");
+    Console.Write(a);
+}
+else if (a<b)
+{
+    Console.Write("Больше число ");
+    Console.Write(b);
+}
+else
+{
+    Console.Write("Числа равны");
+}
