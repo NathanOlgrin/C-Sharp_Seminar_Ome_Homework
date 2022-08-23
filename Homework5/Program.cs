07e2a5211680ed78d440b897ce150a6726cfb33e
Olgrin <matrica_1997@mail.ru>
2022-08-23 22:54:27 +0300

    add Homework 5

---
+++ Homework5/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Ведите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int count = 1;
+if(a>0)
+{
+    while(count<a+1)
+    {
+        if(count%2==0)
+        {
+            Console.Write(count);
+            Console.Write(" ");
+        }
+        count++;
+    }
+}
+else
+{
+    while(count>a-1)
+    {
+        if(count%2==0)
+        {
+            Console.Write(count);
+            Console.Write(" ");
+        }
+        count--;
+    }
+}
