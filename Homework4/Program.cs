1978f774f4e6b295b02b4bcfcf1f5ccfeba15191
Olgrin <matrica_1997@mail.ru>
2022-08-23 22:43:48 +0300

    add Homework 4

---
+++ Homework4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ведите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if(a%2==0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+}
