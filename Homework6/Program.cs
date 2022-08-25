9c039ccd7dc390c04144fe0e201fb15601ad568e
Olgrin <matrica_1997@mail.ru>
2022-08-25 17:16:25 +0300

    add Homework 6

---
+++ Homework6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трёхзначное число: ");
+string string1 = Console.ReadLine();
+Console.Write("Вторая цифра введенного числа: ");
+char number = string1[1];
+Console.WriteLine(number);
