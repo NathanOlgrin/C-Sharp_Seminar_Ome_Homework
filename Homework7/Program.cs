62a377cd30ab142cb620c512df0a2ddf558e17da
Olgrin <matrica_1997@mail.ru>
2022-08-25 17:23:15 +0300

    add Homework 7

---
+++ Homework7/Program.cs
@@ -0,0 +1,25 @@
+﻿int Check(string string2)
+{
+    if(string2.Length<3)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+Console.Write("Введите трёхзначное число: ");
+string string1 = Console.ReadLine();
+
+if(Check(string1)==1)
+{
+    Console.Write("Третья цифра введенного числа: ");
+    char number = string1[2];
+    Console.WriteLine(number);
+}
+else
+{
+    Console.Write("Третьей цифры нет");
+}
