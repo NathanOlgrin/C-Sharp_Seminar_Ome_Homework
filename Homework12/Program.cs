302d0902961299e9b07068d4459b556d0330c88e
Olgrin <matrica_1997@mail.ru>
2022-08-29 20:53:10 +0300

    add Homework 12

---
+++ Homework12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n>0)
+{
+    for(int i=1; i<n+1; i++)
+    {
+        Console.Write(Math.Pow(i, 3));
+        Console.Write(" ");
+    }
+}
+else
+{
+    for (int i=1; i>n-1; i--)
+    {
+        Console.Write(Math.Pow(i, 3));
+        Console.Write(" ");
+    }
+}
