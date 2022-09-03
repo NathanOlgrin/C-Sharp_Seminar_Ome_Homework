7af46b55d864d095d2591f8d62c517844c7caea0
Olgrin <matrica_1997@mail.ru>
2022-09-03 14:25:53 +0300

    add Homework 19

---
+++ Homework19/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Console.WriteLine("Введите количество чисел в массиве: ");
+int quantity = int.Parse(Console.ReadLine());
+int summ = 0;
+int[] array = new int[quantity];
+Console.WriteLine("Вывод массива: ");
+    for (int i=0;i<quantity;i++)
+    {
+        array[i] = new Random().Next(0,100);
+        Console.Write(array[i]);
+        Console.Write(" ");
+        if(i%2!=0)
+        {
+            summ+=array[i];
+        }
+    }
+Console.WriteLine("");
+Console.Write("Сумма нечетных числе в массиве: ");
+Console.WriteLine(summ);
