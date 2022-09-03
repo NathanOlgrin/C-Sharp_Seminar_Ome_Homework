f4a86499d4185081b7462516b1197353dff0ea46
Olgrin <matrica_1997@mail.ru>
2022-09-03 13:51:44 +0300

    add Homework 16 modified

--- Homework16/Program.cs
+++ Homework16/Program.cs
@@ -1,11 +1,14 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 int[] array = new int[8];
-
+Console.Write("[");
 for(int i=0;i<8;i++)
 {
     array[i] = new Random().Next(1,100);
     Console.Write(array[i]);
+    if(i!=7)
+    {
     Console.Write(" ");
+    }
 }
-
+Console.WriteLine("]");
 
