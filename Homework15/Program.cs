9c10fd6f810cf825a529070e3ac5b102f21add29
Olgrin <matrica_1997@mail.ru>
2022-09-03 13:34:17 +0300

    add Homework 15

---
+++ Homework15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int convert_char_to_int(char ch)
+{
+    int x = ch - '0';
+    return x;
+}
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+
+int summ = 0;
+for (int i=0; i<number.Length; i++)
+{
+    summ = summ + convert_char_to_int(number[i]);
+}
+
+Console.Write("Сумма цифр в числе равна: ");
+Console.WriteLine(summ);
