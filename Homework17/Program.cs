795a663a46a5f3bd2d348afa32a4e4cf50952754
Olgrin <matrica_1997@mail.ru>
2022-09-03 13:56:34 +0300

    add Homework 17 modified

--- Homework17/Program.cs
+++ Homework17/Program.cs
@@ -1,9 +1,11 @@
 ﻿// Напишите программу, которая задаёт массив из 10 элементов, которые необходимо заполнить случайными значениями в диапазоне от -10 до 10 и найти максимальное значение среди них
 int[] array = new int[10];
 Console.Write("[");
+int max=-100;
 for(int i=0;i<10;i++)
 {
     array[i] = new Random().Next(-10,10);
+    if(max<array[i]) max = array[i];
     Console.Write(array[i]);
     if(i!=9)
     {
@@ -11,10 +13,6 @@ for(int i=0;i<10;i++)
     }
 }
 Console.WriteLine("]");
-int max=-100;
-for(int i=0;i<10;i++)
-{
-    if(max<array[i]) max = array[i];
-}
+
 Console.Write("Максимальное значение массива: ");
 Console.WriteLine(max);
