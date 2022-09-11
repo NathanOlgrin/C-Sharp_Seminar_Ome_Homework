d7e67dece4ade0acd68714a64c74316f6eed8032
Olgrin <matrica_1997@mail.ru>
2022-09-11 16:48:54 +0300

    add Homework 25

---
+++ Homework25/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+void FillArray(double[,] arr, int line, int col) //arr - array, line - row, col - column
+{
+    Random r = new Random();
+    for(int i=0; i<line; i++)
+    {
+        for(int j=0; j<col;j++)
+        {
+            
+            arr[i,j] = Convert.ToDouble(r.Next(-100,100))/10.0;
+        }
+    }
+}
+
+void PrintArray(double[,] arr, int line, int col) //arr - array
+{
+    for(int i=0; i<line; i++)
+    {
+        for(int j=0; j<col;j++)
+        {
+            Console.Write(arr[i,j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int column = int.Parse(Console.ReadLine());
+
+double[,] array = new double[row,column];
+
+FillArray(array, row, column);
+PrintArray(array, row, column);
