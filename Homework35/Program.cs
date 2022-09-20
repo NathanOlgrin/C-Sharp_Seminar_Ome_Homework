0b7f9e2f8461f7bc14b96ad40e873d74eff51b28
Olgrin <matrica_1997@mail.ru>
2022-09-20 18:02:27 +0300

    add Homework 35

---
+++ Homework35/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+int recursion(int i, int j)
+{
+    if(i>j)
+    {
+        if(i==j)
+        {
+            return i;
+        }
+        return i+recursion(i-1,j);
+    }
+    else
+    {
+        if(i==j)
+        {
+            return i;
+        }
+        return i +recursion(i+1,j);
+    }
+}
+
+Console.WriteLine("Введите число M:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(recursion(m,n));
