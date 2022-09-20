f6376d650d72db055964834baa01b8d6c40806ba
Olgrin <matrica_1997@mail.ru>
2022-09-20 18:12:50 +0300

    add Homework 36

---
+++ Homework36/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+Console.WriteLine("Hello, World!");
+int recursion(int i, int j)
+{
+    if(j==0)
+    {
+        return i;
+    }
+    else
+    {
+        return recursion(j, i%j);
+    }
+}
+
+Console.WriteLine("Введите число M:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(recursion(m,n));
